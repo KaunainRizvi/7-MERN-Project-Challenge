b69f92260fc8a3547824b8440e77b33b160a332e
KaunainRizvi <krizvibusiness.com>
2023-12-17 13:04:11 +0530

    client side setup started

---
+++ Job Web-App MERN/client/src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div>
+          <h1>Job Board</h1>
+          <button>Show Job Form</button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
