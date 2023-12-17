518b354018e2de2dd34f6e2954c4dfa1cae6aeb1
KaunainRizvi <krizvibusiness.com>
2023-12-17 17:05:18 +0530

    data base not added yet

--- Job Web-App MERN/server/controller/job-controller.js
+++ Job Web-App MERN/server/controller/job-controller.js
@@ -1,3 +1,14 @@
-export const saveJobData = async () =>{
+import job from "../models/job.js"
+
+
+export const saveJobData = async (request, response) =>{
+try {
+    const newJob = new job(request.body);
+    await newJob.save();
+    response.status(200).json({message:"Job Saved Successfully"})
     
+} catch (error) {
+    console.log(error.message)
+    response.status(500).json({error: error.message})
+}
 }
