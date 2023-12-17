3fb18392f31bac0f098f59c125fe312073d566a7
KaunainRizvi <krizvibusiness.com>
2023-12-17 15:46:23 +0530

    server setup

---
+++ Job Web-App MERN/server/server.js
@@ -0,0 +1,14 @@
+import express  from "express";
+import router from "./routes/route.js";
+import bodyParser from "body-parser";
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json({extended : true}))
+app.use('/', router)
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, ()=> console.log("Server is running on PORT ${PORT}"));
